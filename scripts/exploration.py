f78e2b6c6ff264fc11593ef4f8d513c7b465a7fd
NotsoJharedtroll23 <abraham.flores193@tectijuana.edu.mx>
2023-09-22 21:41:20 -0700

    REDACT: histogramas preliminares para datos demograficos

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -1,25 +1,41 @@
 import pandas
+import numpy
+
 
 def exploration(csv_file):
     df = pandas.read_csv(csv_file)
 
-    # Obtener las dimensiones de los datos (filas x columnas)
+    # * Obtener las dimensiones de los datos (filas x columnas)
     filas, columnas = df.shape
-    
-    print("\tEXPLORACION INICIAL DE DATOS")
-    print(f'Ruta del archivo explorado: {csv_file}\n')
-    print(f'Dimensiones de los datos: {filas} filas x {columnas} columnas\n')
 
-    # Imprimir los nombres de las columnas
+    print("\tEXPLORACION INICIAL DE DATOS")
+    print(f"Ruta del archivo explorado: {csv_file}\n")
+    print(f"Dimensiones de los datos: {filas} filas x {columnas} columnas\n")
+
+    # * Imprimir los nombres de las columnas
     nombres_columnas = df.columns.tolist()
-    print('Nombres de las columnas:')
+
+    print("Nombres de las columnas:")
     for idx, nombre in enumerate(nombres_columnas):
-        print(f'•{idx+1}: {nombre}')
+        print(f"•{idx+1}: {nombre}")
+
+    # * Prueba de acceso de nombre_columnas por medio de indices
+    print(nombres_columnas[3])
+
+    # * prueba de tipo de datos desplegados
+    datos = df.iloc[:, 3]
+    datos = datos.value_counts()
+    datos = datos.values
+
+    print(datos)
+    print(numpy.array(datos.tolist()))
+
 
 def main():
-    CSV_FILE = '.\csv\EncuestaPreliminar.csv'
-    
+    CSV_FILE = "../csv/EncuestaPreliminar.csv"
+
     exploration(CSV_FILE)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- scripts/plots.py
+++ scripts/plots.py
@@ -5,45 +5,69 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({"figure.facecolor": (1, 1, 1, 0)})
 
 
-# TODO histogramas de las preguntas demográficas
-def histogram(csv_file, columna_csv, opciones):
-    df = pandas.read_csv(csv_file)
-    datos = df.iloc[:, columna_csv]
-    contador_incidencias = [datos.count(opcion) for opcion in opciones]
-    etiquetas = opciones
+# TODO alternativa de despliegue de opciones muy largas como anidado en leyenda
+def histogram(df, columna_csv, opciones):
+    colores = ["royalblue", "darkorange", "limegreen", "mediumpurple", "indianred"]
+    nombre_archivo = f"histograma_preguntacolumna{columna_csv}"
+    nombre_pregunta = df.columns.tolist()
+    datos = df.iloc[:, columna_csv - 1]
 
+    # * Filtra los datos para mantener solo las opciones deseadas
+    datos_filtrados = datos[datos.isin(opciones)]
+
+    # * Cuenta las incidencias de cada opción
+    conteo = datos_filtrados.value_counts().reindex(opciones, fill_value=0)
+
+    # * Parametros para graficos
     fig, axes = plt.subplots(figsize=(10, 6))
-    bars = axes.bar(etiquetas, contador_incidencias, alpha=0.7)
+    bars = axes.bar(conteo.index, conteo.values, alpha=0.7, color=colores)
     axes.set_xlabel("Opciones de Respuesta")
     axes.set_ylabel("Conteo")
-    axes.set_title(f"Histograma de Opciones Pregunta DEMOGRAFICA {columna_csv}")
+    axes.set_title(f"{nombre_pregunta[columna_csv-1]}")
 
-    plt.savefig(f".\\results\plots\histograma_preguntademografica{columna_csv}")
-    n_filas, n_columnas = df.shape
+    # * Agregar números en las barras
+    for bar, count in zip(bars, conteo):
+        axes.text(
+            bar.get_x() + bar.get_width() / 2,
+            bar.get_height(),
+            str(count),
+            ha="center",
+            va="bottom",
+        )
+
+    plt.savefig(f"../results/plots/{nombre_archivo}")
+    print(f"GRAFICA {nombre_archivo} realizada con éxito!")
 
 
 def main():
-    CSV_FILE = ".\csv\EncuestaPreliminar.csv"
+    CSV_FILE = "../csv/EncuestaPreliminar.csv"
+    df_csv = pandas.read_csv(CSV_FILE)
 
-    opciones_pregunta3 = ["17", "18", "19", "20", "21 o mas"]
-    opciones_pregunta4 = ["Alta", "Media Alta", "Media", "Media Baja", "Baja"]
+    opciones_pregunta3 = ["17", "18", "19", "20", "21 o más"]
+    opciones_pregunta4 = [
+        "Clase Alta",
+        "Clase Media Alta",
+        "Clase Media",
+        "Clase Media Baja",
+        "Clase Baja",
+    ]
     opciones_pregunta5 = [
         "Primaria terminada o trunca",
         "Secundaria terminada o trunca",
-        "Prepatoria/Bachillerato tecnico terminado o trunco",
+        "Prepatoria/Bachillerato Técnico terminado o trunco",
         "Licenciatura terminada o trunca",
-        "Posgrados (Maestrias, Doctorados, etc.)",
+        "Posgrados (Maestría, Doctorado, etc.)",
     ]
     opciones_pregunta6 = opciones_pregunta5
-    opciones_pregunta7 = ["Si", "No"]
+    opciones_pregunta7 = ["Sí", "No"]
     opciones_pregunta29 = ["Mujer", "Hombre", "No-binary", "Prefiero no decir"]
 
-    histogram(CSV_FILE, 3, opciones_pregunta3)
-    histogram(CSV_FILE, 4, opciones_pregunta4)
-    histogram(CSV_FILE, 5, opciones_pregunta5)
-    histogram(CSV_FILE, 6, opciones_pregunta6)
-    histogram(CSV_FILE, 7, opciones_pregunta7)
-    histogram(CSV_FILE, 29, opciones_pregunta29)
+    histogram(df_csv, 3, opciones_pregunta3)
+    histogram(df_csv, 4, opciones_pregunta4)
+    histogram(df_csv, 5, opciones_pregunta5)
+    histogram(df_csv, 6, opciones_pregunta6)
+    histogram(df_csv, 7, opciones_pregunta7)
+    histogram(df_csv, 29, opciones_pregunta29)
 
 
 if __name__ == "__main__":
